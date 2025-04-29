afac14c2a7ca0630b95b9529200607cd13955830
fredmunene <munene2050gitonga@gmail.com>
2025-04-29 10:25:56 +0300

    add company sign-up form

--- users/forms.py
+++ users/forms.py
@@ -80,8 +80,62 @@ class CustomerSignUpForm(UserCreationForm):
         return user
 
 
+
 class CompanySignUpForm(UserCreationForm):
-    pass
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Username'}),
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only." 
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter Email'}),
+        help_text="Enter a valid email address."
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter Password'}),
+          help_text=mark_safe(  # Ensures the list is rendered properly
+            "<ul>"
+            "<li><span>Your password can’t be too similar to your other personal information.</span></li>"
+            "<li><span>Your password must contain at least 8 characters.</span></li>"
+            "<li><span>Your password can’t be a commonly used password.</span></li>"
+            "<li><span>Your password can’t be entirely numeric.</span></li>"
+            "</ul>"
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+        help_text="Enter the same password as before, for verification."
+    )
+    field = forms.ChoiceField(
+        choices=Company._meta.get_field('field').choices,  #  Dynamically get choices
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=True
+    )
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'field']
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Username already exists.")
+        return username
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already exists.")
+        return email
+    @transaction.atomic
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_company = True  
+        if commit:
+            user.save()
+            company = Company.objects.create(
+                user=user,
+                field=self.cleaned_data['field'],
+            )
+            company.save()
+        return user
 
 
 class UserLoginForm(forms.Form):
